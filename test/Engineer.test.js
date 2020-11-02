1f06cc231e020298e06e8011d847bb049f3d5305
Derek Thorpe <derekthorpe13@gmail.com>
2020-11-01 21:07:32 -0500

    updated test

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -1,19 +1,19 @@
 const Engineer = require("../lib/Engineer");
 
 test("Can set GitHUb account via constructor", () => {
-  const testValue = "GitHubUser";
-  const e = new Engineer("Foo", 1, "test@test.com", testValue);
-  expect(e.github).toBe(testValue);
+  const value = "GitHubUser";
+  const em = new Engineer("Foo", 1, "test@test.com", value);
+  expect(em.github).toBe(value);
 });
 
 test("getRole() should return \"Engineer\"", () => {
-  const testValue = "Engineer";
-  const e = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
-  expect(e.getRole()).toBe(testValue);
+  const value = "Engineer";
+  const em = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
+  expect(em.getRole()).toBe(value);
 });
 
 test("Can get GitHub username via getGithub()", () => {
-  const testValue = "GitHubUser";
-  const e = new Engineer("Foo", 1, "test@test.com", testValue);
-  expect(e.getGithub()).toBe(testValue);
+  const value = "GitHubUser";
+  const em = new Engineer("Foo", 1, "test@test.com", value);
+  expect(em.getGithub()).toBe(value);
 });
